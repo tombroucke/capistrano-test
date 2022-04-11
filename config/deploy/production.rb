d970cf1c1708551da98c51bce62bf635afc68a9b
Tom Broucke <tom@tombroucke.be>
2022-04-11 17:22:59 +0200

    change cap branch

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,6 +1,6 @@
 set :stage, :production
 set :deploy_to, "/data/sites/web/otomatiesbe/capistrano-test"
-set :branch, 'master'
+set :branch, 'without-composer'
 
 # Simple Role Syntax
 # ==================
